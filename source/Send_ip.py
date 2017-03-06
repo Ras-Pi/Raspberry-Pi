a862de728121198f3ece7a0d031ce918c33b487d
Fanzhenyu <fzyline@163.com>
2017-03-06 20:54:32 +0800

    update Send_ip.py

--- source/Send_ip.py
+++ source/Send_ip.py
@@ -1,17 +1,19 @@
-# -*- coding: utf-8 -*-
+#-*-coding: utf-8-*-
 
-from email import encoders
-from email.header import Header
-from email.mime.text import MIMEText
-from email.utils import parseaddr, formataddr
+import socket
+import time
 import smtplib
+import urllib
+from email.mime.text import MIMEText
 
-# 发送邮件
-def sendEmail(smtpserver,username,password,sender,receiver,subject,msghtml):
-    msg = MIMEText(msghtml, 'plain', 'utf-8')
+#发送邮件的基本函数，参数依次如下
+# smtp服务器地址、邮箱用户名，邮箱秘密，发件人地址，收件人地址（列表的方式），邮件主题，邮件内容
+def sendEmail(smtpserver,username,password,sender,receiver,subject,content):
+    msg = MIMEText(content, 'plain', 'utf-8')
     msg['From'] = sender
     msg['To'] = receiver
     msg['Subject'] = subject
+
     server = smtplib.SMTP(smtpserver, 25)
     server.set_debuglevel(1)
     server.login(username, password)
@@ -22,11 +24,11 @@ def sendEmail(smtpserver,username,password,sender,receiver,subject,msghtml):
 def check_network():
     while True:
         try:
-            result=urllib.urlopen('http://www.baidu.com').read()
+            result=urllib.urlopen('http://baidu.com').read()
             print result
             print "Network is Ready!"
             break
-        except Exception , e:
+        except Exception as e:
             print e
             print "Network is not ready,Sleep 5s..."
             time.sleep(5)
@@ -44,3 +46,4 @@ if  __name__ == '__main__' :
     check_network()
     ipaddr= get_ip_address()
     sendEmail("邮件服务器地址，例如：smtp.163.com",'发送者的邮箱地址','发送者的邮箱密码','发送者邮箱','接收者邮箱','IP Address of Raspberry PI',ipaddr)
+    
